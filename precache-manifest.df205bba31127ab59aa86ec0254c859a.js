2cda431daa3ceb2cc0f4023fcb93870917e0b12e
dinesh3596 <s_dineshkumar@outlook.com>
2020-07-26 11:17:27 +0530

    Updates

---
+++ precache-manifest.df205bba31127ab59aa86ec0254c859a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80aa35ce8a1818aacd9c5f7890994628",
+    "url": "/index.html"
+  },
+  {
+    "revision": "991b21a84d7eb225f0e0",
+    "url": "/static/css/main.91bd37e3.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "991b21a84d7eb225f0e0",
+    "url": "/static/js/main.c045611e.chunk.js"
+  },
+  {
+    "revision": "4708b13b98379c58638d",
+    "url": "/static/js/runtime-main.03d56121.js"
+  }
+]);
